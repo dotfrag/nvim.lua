2d91b3d919785cec5f453f03220103f99d68ade1
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-03-13 20:28:09 +0200

    organise tree-sitter

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -2,17 +2,14 @@ return {
   -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+  opts = {
+    ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
 
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
