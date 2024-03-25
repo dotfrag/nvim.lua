ccdbf5261201a13ce717890e40f7983b6f2e19dd
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-03-25 16:29:12 +0200

    disable rest temporarily

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,5 +1,7 @@
 return {
   "rest-nvim/rest.nvim",
+  -- temp disable until config is updated to match latest version
+  enabled = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>cr", ft = "http", "<Plug>RestNvim", desc = "Run request" },
